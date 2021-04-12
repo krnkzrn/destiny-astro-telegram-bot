54fcfebb672812aaffa41476e23a40d05296edc1
karinakazaryan <karina.kazaryan@gmail.com>
2021-04-12 18:27:07 +0300

    First step for bot

--- bot.py
+++ bot.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 # import redis
 import os
-import telebot
+# import telebot
 
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
 # some_api_token = os.environ['SOME_API_TOKEN']
 
 #       Your bot code below
-bot = telebot.TeleBot(token)
+# bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start', 'go'])
-def start_handler(message):
-    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я стану умнее')
+# @bot.message_handler(commands=['start', 'go'])
+# def start_handler(message):
+#     bot.send_message(message.chat.id, 'Привет, когда я вырасту, я стану умнее')
 
 
-bot.polling()
+# bot.polling()
+print(token)
