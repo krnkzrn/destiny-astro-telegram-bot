9d8987e0c49442ab5d02e8aaaab2c65fe8eb8017
karinakazaryan <karina.kazaryan@gmail.com>
2021-04-19 11:41:42 +0300

    Making dialog with client

--- bot.py
+++ bot.py
@@ -5,16 +5,27 @@ import telebot
 import schedule
 import time
 import threading
+import datetime
 # import fileprocessor
+
+class User:
+    name=''
+    birthdate=datetime.date.today()
+    userid=''
 
 #           Config vars
 token = os.environ['TELEGRAM_TOKEN']
 
 #       Your bot code below
 bot = telebot.TeleBot(token)
+user = User()
+is_dialog_in_progress = False
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
+    print_start_dialog(message.chat.id)
+
+def print_start_dialog(message_chat_id):
     markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.row('Зарегистрироваться')
     markup.row('Оформить подписку')
@@ -22,14 +33,33 @@ def start_handler(message):
     markup.row('Помощь')
     markup.row('Связаться с нами')
 
-    msg = bot.send_message(message.chat.id, 'Здравствуйте, чем я могу вам помочь?', reply_markup=markup)
-    print(message)
+    is_dialog_in_progress = True
+    msg = bot.send_message(message_chat_id, 'Чем я могу вам помочь? В любой момент вы можете прервать диалог с помощью команды /end', reply_markup=markup)
+    # print(message)
 
-    bot.register_next_step_handler(msg)
+@bot.message_handler(commands=['end'])
+def reser_dialog(message):
+    user = User()
+    is_dialog_in_progress = False
+    bot.send_message(message.chat.id, 'Что бы снова начать диалог используейте команду /start')
 
 @bot.message_handler(regexp=['Зарегистрироваться'])
-def start_register(message):
-    bot.send_message(message.chat.id, 'Введите свои фамилию')
+def register_name(message):
+    if is_dialog_in_progress :
+        user = User()
+        msg = bot.send_message(message.chat.id, 'Введите свои ФИО')
+        bot.register_next_step_handler(msg, register_save_name)
+
+def register_save_name(message):
+    user.name = datetime.datetime(message.text)
+    msg = bot.send_message(message.chat.id, 'Введите дату рождения')
+    bot.register_next_step_handler(msg, register_birthday)
+
+def register_birthday(message):
+    user.birthdate = datetime.date(message.text)
+    user.userid = message.from_user.id
+    bot.send_message(message.chat.id, 'Спасибо, {name}, вы успешно зарегистрированы с id {id}.'.format(name=user.name,id=user.userid))
+    print_start_dialog(message.chat.id)
 
 def echoToChat():
     if os.environ['PUBLISH_UPDATES'] == 'TRUE':
